6812c7cfab3d7937b89c682ec40251a373fd42fc
RiskahFadillah <64517694+RiskahFadillah@users.noreply.github.com>
2024-05-31 20:10:02 +0700

    model submission title

---
+++ app/Models/TitleSubmission.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * Created by Reliese Model.
+ */
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * Class TitleSubmission
+ * 
+ * @property int $id
+ * @property string $title
+ * @property int|null $status
+ * @property Carbon $date
+ * @property int $pic
+ * @property int $mahasiswas_id
+ * @property int $pembimbing_1
+ * @property int $pembimbing_2
+ * @property string $konsentrasi_ilmu
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property string|null $deleted_at
+ * 
+ * @property Mahasiswa $mahasiswa
+ * @property Lecture $lecture
+ *
+ * @package App\Models
+ */
+class TitleSubmission extends Model
+{
+	use SoftDeletes;
+	protected $table = 'title_submission';
+
+	protected $casts = [
+		'status' => 'int',
+		'date' => 'datetime',
+		'pic' => 'int',
+		'mahasiswas_id' => 'int',
+		'pembimbing_1' => 'int',
+		'pembimbing_2' => 'int'
+	];
+
+	protected $fillable = [
+		'title',
+		'status',
+		'date',
+		'pic',
+		'mahasiswas_id',
+		'pembimbing_1',
+		'pembimbing_2',
+		'konsentrasi_ilmu'
+	];
+
+	public function mahasiswa()
+	{
+		return $this->belongsTo(Mahasiswa::class, 'mahasiswas_id');
+	}
+
+	public function lecture()
+	{
+		return $this->belongsTo(Lecture::class, 'pic');
+	}
+
+	public function pembimbing1()
+	{
+		return $this->belongsTo(Lecture::class, 'pembimbing_1');
+	}
+
+	public function pembimbing2()
+	{
+		return $this->belongsTo(Lecture::class, 'pembimbing_2');
+	}
+}
---
+++ database/migrations/2024_05_31_194921_create_title_submission_table.php
@@ -0,0 +1,66 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('title_submission', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->tinyInteger('status')->nullable();
+            $table->timestamp('date');
+            $table->foreignId('pic');
+            $table->foreignId('mahasiswas_id');
+            $table->foreignId('pembimbing_1');
+            $table->foreignId('pembimbing_2');
+            $table->string('konsentrasi_ilmu');
+            
+
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('mahasiswas_id')
+                  ->references('id')
+                  ->on('mahasiswas')
+                  ->restrictOnUpdate()
+                  ->restrictOnDelete();
+
+            $table->foreign('pic')
+                ->references('id')
+                ->on('lecture')
+                ->restrictOnUpdate()
+                ->restrictOnDelete();
+
+            $table->foreign('pembimbing_1')
+            ->references('id')
+            ->on('lecture')
+            ->restrictOnUpdate()
+            ->restrictOnDelete();
+
+            $table->foreign('pembimbing_2')
+            ->references('id')
+            ->on('lecture')
+            ->restrictOnUpdate()
+            ->restrictOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('title_submission');
+    }
+};
